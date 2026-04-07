9a9fc8dd40b7333a6c1b44c9ef7df305905e75e9
Engelbert Richelieu Tsinda <engelbertrichelieutsinda@gmail.com>
2026-04-07 01:53:34 +0100

    Gestion des payments don

---
+++ config/campay.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    // Sandbox
+    'url' => env('CAMPAY_URL'),
+    'username' => env('CAMPAY_USERNAME'),
+    'password' => env('CAMPAY_PASSWORD'),
+
+    // Production
+    'prod_url' => env('CAMPAY_PROD_URL'),
+    'prod_username' => env('CAMPAY_PROD_USERNAME'),
+    'prod_password' => env('CAMPAY_PROD_PASSWORD'),
+
+];
